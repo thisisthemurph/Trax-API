2343f8fb4cf636535c9d6722bf398266f54ec68f
= <michael.w.murphy@outlook.com>
2020-07-16 05:56:43 +0100

    Refactor code styling

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to the Trax API!' })
+router.get("/", (req, res) => {
+	res.json({ msg: "Welcome to the Trax API!" })
 })
 
 module.exports = router
